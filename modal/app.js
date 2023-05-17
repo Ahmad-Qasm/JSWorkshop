955e46344b212ca651913d1c7697e979ea5d019f
ahmad qassim <amqsm.93@gmail.com>
2023-05-17 23:28:45 +0200

    added a modal project

---
+++ modal/app.js
@@ -0,0 +1,17 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modal_btn = document.querySelector(".modal-btn");
+const modal_overlay = document.querySelector(".modal-overlay");
+const close_btn = document.querySelector(".close-btn");
+
+modal_btn.addEventListener("click", () => {
+  console.log(modal_overlay.classList);
+  modal_overlay.classList.add("open-modal");
+});
+
+close_btn.addEventListener("click", () => {
+  modal_overlay.classList.remove("open-modal");
+});
