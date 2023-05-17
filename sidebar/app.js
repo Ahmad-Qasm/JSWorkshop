4b6e87727d23c1da19956d61e7446cb01a0c719b
ahmad qassim <amqsm.93@gmail.com>
2023-05-17 16:55:18 +0200

    Added the sidebar project.

---
+++ sidebar/app.js
@@ -0,0 +1,11 @@
+const sidebar_toggle = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const close_btn = document.querySelector(".close-btn");
+
+sidebar_toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+close_btn.addEventListener("click", () => {
+  sidebar.classList.toggle("show-sidebar");
+});
